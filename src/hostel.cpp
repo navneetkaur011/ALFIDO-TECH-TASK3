60f27ebafcdc3242ccad6b1f207eb308cef11b67
navneetkaur011 <navvkaurnanda@gmail.com>
2026-07-19 17:25:21 +0530

    Update hostel.cpp implementation with polymorphic displayInfo logic

--- src/hostel.cpp
+++ src/hostel.cpp
@@ -1,9 +1,10 @@
-#include "../include/Hostel.h"
+#include "../include/hostel.h"
 #include <iostream>
+
 using namespace std;
 
 void UniversityHostel::displayInfo() const {
-    cout << "\nHostel Name: " << hostelName << endl;
-    cout << "Facilities : " << facilities << endl;
-    cout << "Total Rooms: " << totalRooms << endl;
+    cout << "\n🏠 Hostel Name: " << hostelName << endl;
+    cout << "⚡ Facilities : " << facilities << endl;
+    cout << "🛏️ Total Rooms: " << totalRooms << endl;
 }
